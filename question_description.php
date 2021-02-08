78f6954d843c4062c134aebeacfaa92cb888db94
Mohammed Saduzzaman Sadi <64192420+zamans78@users.noreply.github.com>
2021-02-08 17:02:12 +0600

    Update question_description.php
    Design and functionality of create question Changed.

--- question_description.php
+++ question_description.php
@@ -4,17 +4,17 @@ require_once "assets/connect/pdo.php";
 include 'assets/inc/validation_helper.php';
 
 if (!isset($_SESSION['Teacher_ID'])) {
-	die("Not logged in");
+    die("Not logged in");
 }
 
 $status = false;
 
 if (isset($_SESSION['status'])) {
-	$status = htmlentities($_SESSION['status']);
-	$status_color = htmlentities($_SESSION['color']);
+    $status = htmlentities($_SESSION['status']);
+    $status_color = htmlentities($_SESSION['color']);
 
-	unset($_SESSION['status']);
-	unset($_SESSION['color']);
+    unset($_SESSION['status']);
+    unset($_SESSION['color']);
 }
 
 $_SESSION['color'] = 'red';
@@ -22,55 +22,55 @@ $_SESSION['color'] = 'red';
 // Check to see if we have some POST data, if we do process it
 if (isset($_POST['Course_Code']) && isset($_POST['Course_Name']) && isset($_POST['Batch']) && isset($_POST['Section']) && isset($_POST['Title'])) {
 
-	if (!validationHelper()) {
-		header("Location: question_description.php");
-		return;
-	}
+    if (!validationHelper()) {
+        header("Location: question_description.php");
+        return;
+    }
 
-	$Course_Code = htmlentities($_POST['Course_Code']);
-	$Course_Name = htmlentities($_POST['Course_Name']);
-	$Batch = htmlentities($_POST['Batch']);
-	$Section = htmlentities($_POST['Section']);
-	$Title = htmlentities($_POST['Title']);
+    $Course_Code = htmlentities($_POST['Course_Code']);
+    $Course_Name = htmlentities($_POST['Course_Name']);
+    $Batch = htmlentities($_POST['Batch']);
+    $Section = htmlentities($_POST['Section']);
+    $Title = htmlentities($_POST['Title']);
 
-	$stmt = $pdo->prepare("
+    $stmt = $pdo->prepare("
         INSERT INTO question_description (Teacher_ID, Course_Code, Course_Name, Batch, Section, Title)
         VALUES (:Teacher_ID, :Course_Code, :Course_Name, :Batch, :Section, :Title)
     ");
 
-	$stmt->execute([
-		':Teacher_ID' => $_SESSION['Teacher_ID'],
-		':Course_Code' => $Course_Code,
-		':Course_Name' => $Course_Name,
-		':Batch' => $Batch,
-		':Section' => $Section,
-		':Title' => $Title,
-	]);
+    $stmt->execute([
+        ':Teacher_ID' => $_SESSION['Teacher_ID'],
+        ':Course_Code' => $Course_Code,
+        ':Course_Name' => $Course_Name,
+        ':Batch' => $Batch,
+        ':Section' => $Section,
+        ':Title' => $Title,
+    ]);
 
-	$Question_Description_ID = $pdo->lastInsertId();
+    $Question_Description_ID = $pdo->lastInsertId();
 
-	for ($i = 1; $i <= 50; $i++) {
-		if (!isset($_POST['Question' . $i])) {
-			continue;
-		}
-		$Question = htmlentities($_POST['Question' . $i]);
+    for ($i = 1; $i <= 50; $i++) {
+        if (!isset($_POST['Question' . $i])) {
+            continue;
+        }
+        $Question = htmlentities($_POST['Question' . $i]);
 
-		$stmt = $pdo->prepare("
+        $stmt = $pdo->prepare("
             INSERT INTO question (Question_Description_ID, Question)
             VALUES (:Question_Description_ID,:Question)
         ");
 
-		$stmt->execute([
-			':Question_Description_ID' => $Question_Description_ID,
-			':Question' => $Question,
-		]);
-	}
+        $stmt->execute([
+            ':Question_Description_ID' => $Question_Description_ID,
+            ':Question' => $Question,
+        ]);
+    }
 
-	$_SESSION['status'] = 'Record added';
-	$_SESSION['color'] = 'green';
+    $_SESSION['status'] = 'Record added';
+    $_SESSION['color'] = 'green';
 
-	header('Location: teacher_dashboard.php');
-	return;
+    header('Location: teacher_dashboard.php');
+    return;
 }
 
 ?>
@@ -79,8 +79,8 @@ if (isset($_POST['Course_Code']) && isset($_POST['Course_Name']) && isset($_POST
 
 <head>
 	<?php
-	require_once 'assets/connect/head.php';
-	?>
+require_once 'assets/connect/head.php';
+?>
 </head>
 
 <body>
@@ -103,18 +103,18 @@ if (isset($_POST['Course_Code']) && isset($_POST['Course_Name']) && isset($_POST
 			</div>
 			<div class="col d-flex justify-content-center mt-3">
 
-				
+
 
 			</div>
 		</div>
 		<?php
-		if ($status !== false) {
-			// Look closely at the use of single and double quotes
-			echo ('<p style="color: ' . $status_color . ';" class="col-sm-10 col-sm-offset-2">' .
-				htmlentities($status) .
-				"</p>\n");
-		}
-		?>
+if ($status !== false) {
+    // Look closely at the use of single and double quotes
+    echo ('<p style="color: ' . $status_color . ';" class="col-sm-10 col-sm-offset-2">' .
+        htmlentities($status) .
+        "</p>\n");
+}
+?>
 		<div class="container col-xl-7 col-lg-7 col-md-7">
 			<form method="post" class="form-horizontal">
 				<div class="form-group input-group input-group-lg">
@@ -175,42 +175,38 @@ if (isset($_POST['Course_Code']) && isset($_POST['Course_Name']) && isset($_POST
 		</div>
 	</div>
 
-	<script>
-		countPos = 0;
 
-		// http://stackoverflow.com/questions/17650776/add-remove-html-inside-div-using-javascript
-		$(document).ready(function() {
-			window.console && console.log('Document ready called');
-			$('#addPos').click(function(event) {
-				// http://api.jquery.com/event.preventdefault/
-				event.preventDefault();
-				if (countPos >= 50) {
-					alert("Maximum of nine position entries exceeded");
-					return;
-				}
-				countPos++;
-				window.console && console.log("Adding position " + countPos);
+<script type="text/javascript">
+let countPos = 0;
+$(document).ready(function(){
+$('#addPos').click(function(event){
+    event.preventDefault();
 
-				$('#position_fields').append(
-					'<div class="pt-5" id="position' + countPos + '">\
-   <div class="form-group "> \
-   <div class="col"> \
-   <button class="btn btn-danger btn-block" \
-   onclick="$(\'#position' + countPos + '\').remove();return false;" \
-   ><i class="fas fa-eraser"></i></button> \
-   </div> \
-   </div> \
-   <div class="form-group m-0 p-0"> \
-   <label class="control-label col-sm-2"></label> \
-   <div class="col"> \
-   <textarea class="form-control" name="Question' + countPos + '" rows="4" ></textarea> \
-   </div> \
-   </div> \
-   </div>'
-				);
-			});
-		});
-	</script>
+    if (countPos >= 11) {
+    alert("Maximum of 10 position entries exceeded");
+    return;
+}
+countPos++;
+
+let html = '';
+html += '<div id="inputFormRow">';
+html += '<div class="input-group mb-3">';
+html += '  <textarea class="form-control m-input" name="Question' + countPos + '" placeholder="Q..." id="floatingTextarea2" style="height: 100px" autocomplete="off"></textarea>';
+html += '<div class="input-group-append">';
+html += '<button id="removeRow" type="button" class="ml-2 btn btn-danger">Remove</button>';
+html += '</div>';
+html += '</div>';
+
+$('#position_fields').append(html);
+
+$(document).on('click', '#removeRow', function () {
+$(this).closest('#inputFormRow').remove();
+});
+
+});
+});
+
+</script>
 </body>
 <?php
 require_once 'assets/connect/footer.php';

b8185bf54b6bc34eac941e2eee93b4ba6a665ad4
Mohi Uddin Pabel <mup.pabel1212@gmail.com>
2021-02-09 13:32:48 +0600

    Logo added near the title.
    Added LuExaHiveLogo.png near the page title.

--- assets/connect/head.php
+++ assets/connect/head.php
@@ -11,7 +11,7 @@
  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous" />
 
  <!-- Lu Logo near title-->
- <link rel="icon" type="image/png" href="assets/images/h_logo.png" />
+ <link rel="icon" type="image/png" href="assets/images/LuExamHiveLogo.png" />
  <title>LU Exam Hive</title>
 
  <!-- Custom CSS fike -->

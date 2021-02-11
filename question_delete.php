68d308a232ee863a85998582de20b5d960a625b1
Mohi Uddin Pabel <mup.pabel1212@gmail.com>
2021-02-11 22:42:29 +0600

    question_delete page updated
    question_delete page updated

--- question_delete.php
+++ question_delete.php
@@ -20,7 +20,7 @@ if (!isset($_GET['Question_Description_ID'])) {
     return;
 }
 
-$stmt = $pdo->prepare("SELECT Course_Code, Course_Name FROM question_description where Question_Description_ID = :xyz");
+$stmt = $pdo->prepare("SELECT Course_Code, Course_Name, Batch, Section FROM question_description where Question_Description_ID = :xyz");
 $stmt->execute(array(":xyz" => $_GET['Question_Description_ID']));
 $row = $stmt->fetch(PDO::FETCH_ASSOC);
 if ($row === false) {
@@ -38,22 +38,41 @@ if ($row === false) {
     require_once 'assets/connect/head.php';
     ?>
 </head>
-<header>
-    <nav class="navbar navbar-expand-lg navbar-light sticky-top">
-        <div class="container justify-content-start">
-            <a class="navbar-brand" href="index.php"><img src="assets/images/LuExamHiveLogo.png" height="30px"> LU EXAM HIVE</a>
-            <a type="button" href="javascript:history.back(1)" class="btn btn-sm btn-outline-dark ml-3"><i class="fas fa-arrow-left"></i> Go Back</a>
-        </div>
-    </nav>
-</header>
 
 <body>
-    <div class="container">
-        <p>Course Code: <?php echo ($row['Course_Code']); ?></p>
-        <p>Course Name: <?php echo ($row['Course_Name']); ?></p>
-        <form method="post"><input type="hidden" name="Question_Description_ID" value="<?php echo $_GET['Question_Description_ID'] ?>">
-            <input type="submit" name="Delete" value="Delete">
-        </form>
+    <nav class="navbar navbar-expand-lg navbar-light sticky-top">
+            <div class="container justify-content-start">
+                <a class="navbar-brand" href="index.php"><img src="assets/images/LuExamHiveLogo.png" height="30px"> LU EXAM HIVE</a>
+                <a type="button" href="javascript:history.back(1)" class="btn btn-sm btn-outline-dark ml-3"><i class="fas fa-arrow-left"></i> Go Back</a>
+            </div>
+        </nav>
+    </header>
+    <div class="text-center bg-light text-dark ">
+      <div class="card-header alert alert-danger ">
+        <h2 class="display-4">Delete Confirmation</h2>
+      </div>
+      <div class="col d-flex justify-content-center mt-3">
+        <p class="text-danger">File with following information will be deleted permanently.</p>
+      </div>
+
+      <div class="row">
+        <div class="col d-flex justify-content-center mt-3">
+          <p class="deleteText"><b>Course Name:</b> <?php echo ($row['Course_Name']); ?>. <b>Course Code:</b> <?php echo ($row['Course_Code']); ?>.</p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col d-flex justify-content-center mt-3">
+          <p class="deleteText"><b>Batch:</b> <?php echo ($row['Batch']); ?>. <b>Section:</b> <?php echo ($row['Section']); ?>.</p>
+        </div>
+      </div>
+      <div class="row mt-3 mb-5">
+        <div class="col d-flex justify-content-center mt-3">
+          <form method="POST">
+            <input type="hidden" name="Question_Description_ID" value="<?php echo $_GET['Question_Description_ID'] ?>">
+            <input class="btn btn-danger btn-lg" type="submit" name="Delete" value="Delete">
+          </form>
+        </div>
+      </div>
     </div>
     <?php
     require_once 'assets/connect/footer.php';

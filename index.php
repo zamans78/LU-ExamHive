0958a011b6e04cfd18c407cf023062c0efc9a98d
TAT98 <45793820+TAT98@users.noreply.github.com>
2021-02-25 12:17:52 +0600

    FAQ section redesigned
    Accordion is added in faq section

--- index.php
+++ index.php
@@ -278,55 +278,128 @@ require_once 'assets/connect/head.php';
 
 	<div class="container  mt-5 pb-5  class="accordion accordion-flush" id="accordionFlushExample"">
 		<h2 class="display-3 mt-4" id="FAQ">FAQ</h2>
+		<div class="accordion" id="accordionExample">
 		<div class="row mt-4">
-			<div class="col-md-6">
-				<p class="lead"><b>Q. Why them, and I started to write his adventures.</b></p class="lead">
-				<p class="text-justify">In the Nineties, my background as a volunteer in pediatric hospitals with
-					ABIO Association (a
-					charity
-					tender and clumsy mouse, Geronimo Stilton.</p>
+			<div class="col-md-12">
+					<div class="card border-light">
+						<div class="card-header" id="headingOne">
+						<h2 class="mb-0">
+							<button class="btn btn-link btn-block text-left text-decoration-none text-danger" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+							<p class="lead"><b>Q. Why them, and I started to write his adventures.</b></p class="lead">
+							</button>
+						</h2>
+					</div>
+				</div>
+				<div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+					<div class="card-body">
+						<p class="text-justify">In the Nineties, my background as a volunteer in pediatric hospitals with
+							ABIO Association (a
+							charity
+							tender and clumsy mouse, Geronimo Stilton.</p>
+					</div>
+				</div>
 			</div>
-			<div class="col-md-6">
-				<p class="lead"><b>Q. Why them, and I started to write his adventures.</b></p class="lead">
-				<p class="text-justify">In the Nineties, my background as a volunteer in pediatric hospitals with
-					ABIO Association (a
-					charity
-					tender and clumsy mouse, Geronimo Stilton.</p>
-			</div>
+			
 		</div>
-		<div class="row">
-			<div class="col-md-6">
-				<p class="lead"><b>Q. Why them, and I started to write his adventures.</b></p class="lead">
-				<p class="text-justify">In the Nineties, my background as a volunteer in pediatric hospitals with
-					ABIO Association (a
-					charity
-					tender and clumsy mouse, Geronimo Stilton.</p>
-			</div>
-			<div class="col-md-6">
-				<p class="lead"><b>Q. Why them, and I started to write his adventures.</b></p class="lead">
-				<p class="text-justify">In the Nineties, my background as a volunteer in pediatric hospitals with
-					ABIO Association (a
-					charity
-					tender and clumsy mouse, Geronimo Stilton.</p>
-			</div>
 		</div>
-		<div class="row">
-			<div class="col-md-6">
-				<p class="lead"><b>Q. Why them, and I started to write his adventures.</b></p class="lead">
-				<p class="text-justify">In the Nineties, my background as a volunteer in pediatric hospitals with
-					ABIO Association (a
-					charity
-					tender and clumsy mouse, Geronimo Stilton.</p>
+		<div class="accordion" id="accordionExample">
+		<div class="row mt-4">
+			<div class="col-md-12">
+					<div class="card border-light">
+						<div class="card-header" id="headingTwo">
+						<h2 class="mb-0">
+							<button class="btn btn-link btn-block text-left text-decoration-none text-danger" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+							<p class="lead"><b>Q. Why them, and I started to write his adventures.</b></p class="lead">
+							</button>
+						</h2>
+					</div>
+				</div>
+				<div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+					<div class="card-body">
+						<p class="text-justify">In the Nineties, my background as a volunteer in pediatric hospitals with
+							ABIO Association (a
+							charity
+							tender and clumsy mouse, Geronimo Stilton.</p>
+					</div>
+				</div>
 			</div>
-			<div class="col-md-6">
-				<p class="lead"><b>Q. Why them, and I started to write his adventures.</b></p class="lead">
-				<p class="text-justify">In the Nineties, my background as a volunteer in pediatric hospitals with
-					ABIO Association (a
-					charity
-					tender and clumsy mouse, Geronimo Stilton.</p>
+			
+		</div>
+		</div>
+		<div class="accordion" id="accordionExample">
+		<div class="row mt-4">
+			<div class="col-md-12">
+					<div class="card border-light">
+						<div class="card-header" id="headingThree">
+						<h2 class="mb-0">
+							<button class="btn btn-link btn-block text-left text-decoration-none text-danger" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="true" aria-controls="collapseThree">
+							<p class="lead"><b>Q. Why them, and I started to write his adventures.</b></p class="lead">
+							</button>
+						</h2>
+					</div>
+				</div>
+				<div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+					<div class="card-body">
+						<p class="text-justify">In the Nineties, my background as a volunteer in pediatric hospitals with
+							ABIO Association (a
+							charity
+							tender and clumsy mouse, Geronimo Stilton.</p>
+					</div>
+				</div>
 			</div>
+		</div>	
+		</div>
+		<div class="accordion" id="accordionExample">
+		<div class="row mt-4">
+			<div class="col-md-12">
+					<div class="card border-light">
+						<div class="card-header" id="headingFour">
+						<h2 class="mb-0">
+							<button class="btn btn-link btn-block text-left text-decoration-none text-danger" type="button" data-toggle="collapse" data-target="#collapseFour" aria-expanded="true" aria-controls="collapseOne">
+							<p class="lead"><b>Q. Why them, and I started to write his adventures.</b></p class="lead">
+							</button>
+						</h2>
+					</div>
+				</div>
+				<div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordionExample">
+					<div class="card-body">
+						<p class="text-justify">In the Nineties, my background as a volunteer in pediatric hospitals with
+							ABIO Association (a
+							charity
+							tender and clumsy mouse, Geronimo Stilton.</p>
+					</div>
+				</div>
+			</div>
+			
+		</div>
+		</div>
+		<div class="accordion" id="accordionExample">
+		<div class="row mt-4">
+			<div class="col-md-12">
+					<div class="card border-light">
+						<div class="card-header" id="headingFive">
+						<h2 class="mb-0">
+							<button class="btn btn-link btn-block text-left text-decoration-none text-danger" type="button" data-toggle="collapse" data-target="#collapseFive" aria-expanded="true" aria-controls="collapseFive">
+							<p class="lead"><b>Q. Why them, and I started to write his adventures.</b></p class="lead">
+							</button>
+						</h2>
+					</div>
+				</div>
+				<div id="collapseFive" class="collapse" aria-labelledby="headingFive" data-parent="#accordionExample">
+					<div class="card-body">
+						<p class="text-justify">In the Nineties, my background as a volunteer in pediatric hospitals with
+							ABIO Association (a
+							charity
+							tender and clumsy mouse, Geronimo Stilton.</p>
+					</div>
+				</div>
+			</div>
+			
+		</div>
 		</div>
 	</div>
+
+
 	<!--FAQ End -->
 
 
